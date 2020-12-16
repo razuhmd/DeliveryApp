fb6f113c838e4860b18997c5dd455035bd7368cf
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2020-12-16 04:12:21 +0100

    Commit

---
+++ Pages/myPages/DriverLogin.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeliveryApp.Pages.myPages
+{
+    public class DriverLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
